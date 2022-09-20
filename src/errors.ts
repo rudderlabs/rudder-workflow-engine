506681b46acd8e167115bc68278ff9368fe7836c
Dilip Kumar Kola <dilipkola@rudderstack.com>
2022-09-20 09:36:00 +0530

    Fix PR comments

--- src/errors.ts
+++ src/errors.ts
@@ -1,6 +1,6 @@
 export class CustomError extends Error {
-  response: { status: number; stepName: string };
-  constructor(message: string, status: number, stepName: string) {
+  response: { status: number; stepName?: string };
+  constructor(message: string, status: number, stepName?: string) {
     super(message);
     this.response = { status, stepName };
   }
