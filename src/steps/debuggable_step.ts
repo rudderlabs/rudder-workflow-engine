5ae494625da557170760ece9bd57e06cd0821a29
Dilip Kumar Kola <dilipkola@rudderstack.com>
2022-09-25 03:07:57 +0530

    Add conditions and input template scenarios

--- src/steps/debuggable_step.ts
+++ src/steps/debuggable_step.ts
@@ -1,27 +1,19 @@
-import { ExecutionBindings, StepOutput } from "src/types";
-import { DecoratableStepExecutor } from "./decoratable_step";
-import { StepExecutor } from "./interface";
+import { ExecutionBindings, StepOutput } from 'src/types';
+import { DecoratableStepExecutor } from './decoratable_step';
+import { StepExecutor } from './interface';
 
 /**
- * This is to set the break points when step using debug flag
+ * This is to set the break points when step is using debug flag
  */
 export class DebuggableStepExecutor extends DecoratableStepExecutor {
-    constructor(stepExecutor: StepExecutor) {
-        super(stepExecutor);
-    }
+  constructor(stepExecutor: StepExecutor) {
+    super(stepExecutor);
+  }
 
-    shouldSkip(input: any, executionBindings: ExecutionBindings): boolean {
-        return super.shouldSkip(input, executionBindings);
-    }
-
-    prepareInput(input: any, executionBindings: ExecutionBindings): Promise<any> {
-        return super.prepareInput(input, executionBindings);
-    }
-
-    async execute(input: any, executionBindings: ExecutionBindings): Promise<StepOutput> {
-        super.getLogger().debug("input", input);
-        const output = await super.execute(input, executionBindings)
-        super.getLogger().debug("output", output);
-        return output;
-    }
+  async execute(input: any, executionBindings: ExecutionBindings): Promise<StepOutput> {
+    super.getLogger().debug('input', input);
+    const output = await super.execute(input, executionBindings);
+    super.getLogger().debug('output', output);
+    return output;
+  }
 }
