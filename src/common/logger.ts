27245a6d231a0c1d3e6530e01415a2c808765ba2
Dilip Kola <33080863+koladilip@users.noreply.github.com>
2022-10-12 13:56:59 +0530

    feat(logger): add getLogLevel function (#27)
    * feat(logger): add getLogLevel function
    
    Transformer log level environment variables are conflicting with
    pino logger so added getLogLevel function to return valid pino
    log level.
    
    * refactor: rename the test name
    
    Just rename the test for better clarity.

--- src/common/logger.ts
+++ src/common/logger.ts
@@ -1,6 +1,29 @@
 import pino from 'pino';
 import pinoCaller from 'pino-caller';
 
+/**
+ * Returns the pino compatible log level
+ * @returns pino log level
+ */
+function getLogLevel(): string {
+  switch (process.env.LOG_LEVEL) {
+    case 'debug':
+    case '0':
+      return 'debug';
+    case 'info':
+    case '1':
+      return 'info';
+    case 'warn':
+    case '2':
+      return 'warn';
+    case 'error':
+    case '3':
+      return 'error';
+    default:
+      return 'info';
+  }
+}
+
 export function getLogger(name: string): pino.Logger {
-  return pinoCaller(pino({ name, level: process.env.LOG_LEVEL || 'info' }));
+  return pinoCaller(pino({ name, level: getLogLevel() }));
 }
