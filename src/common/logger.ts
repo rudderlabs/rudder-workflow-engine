b76d135db8c1b7f13b68cdab9b53cc5188e5c021
Dilip Kumar Kola <dilipkola@rudderstack.com>
2022-10-12 13:09:27 +0530

    feat(logger): add getLogLevel function
    Transformer log level environment variables are conflicting with
    pino logger so added getLogLevel function to return valid pino
    log level.

--- src/common/logger.ts
+++ src/common/logger.ts
@@ -1,6 +1,25 @@
 import pino from 'pino';
 import pinoCaller from 'pino-caller';
 
+function getLogLevel(): string {
+  switch (process.env.LOG_LEVEL) {
+    case 'debug':
+    case '0':
+      return 'debug';
+    case 'info':
+    case '1':
+      return 'info';
+    case 'warn':
+    case '2':
+      return 'warn';
+    case 'error':
+    case '3':
+      return 'error';
+    default:
+      return 'info';
+  }
+}
+
 export function getLogger(name: string): pino.Logger {
-  return pinoCaller(pino({ name, level: process.env.LOG_LEVEL || 'info' }));
+  return pinoCaller(pino({ name, level: getLogLevel() }));
 }
