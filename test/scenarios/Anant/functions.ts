f1b042dfcec4139708f41bdafacadbcf4720de9e
Anant <anant@rudderstack.com>
2022-10-14 12:27:31 +0530

    onboarding workflow by Anant

---
+++ test/scenarios/Anant/functions.ts
@@ -0,0 +1,6 @@
+export const add = (a, b) => a + b;
+export const subtract = (a, b) => a - b;
+export const multiply = (a, b) => a * b;
+export const divide = (a, b) => a / b;
+export const cald = (a,b,c) => (b*b)-4*a*c;
+export const calFirst =(b,D) => b+D
