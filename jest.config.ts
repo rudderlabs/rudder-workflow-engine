02c2a3f192a38df17d81b4ae0c3ec1f87249528c
saikumarrs <saibattinoju@rudderstack.com>
2022-11-04 14:26:28 +0530

    chore: exclude external_dependencies directory from coverage

--- jest.config.ts
+++ jest.config.ts
@@ -26,7 +26,7 @@ export default {
   coverageDirectory: 'coverage',
 
   // An array of regexp pattern strings used to skip coverage collection
-  coveragePathIgnorePatterns: ['/node_modules/', '/build/', '__tests__'],
+  coveragePathIgnorePatterns: ['/node_modules/', '/build/', '__tests__', 'src/external_dependencies/'],
 
   // Indicates which provider should be used to instrument code for coverage
   coverageProvider: 'v8',
