ec5aab94110225f13a3740a6e4f7b444e5aca3a8
Dilip Kola <kdilipkola@gmail.com>
2023-01-20 09:20:45 +0530

    feat: add mutation testing support
    Ref: https://stryker-mutator.io/docs/stryker-js/getting-started/

--- test/e2e-custom.test.ts
+++ test/e2e-custom.test.ts
@@ -5,7 +5,7 @@ const rootDirName = 'custom_scenarios';
 
 describe('Custom Scenarios tests', () => {
   describe('loop over input', () => {
-    it('should return orginal error when error wrapped', async () => {
+    it('should return original error when error wrapped', async () => {
       const scenarioDir = join(__dirname, rootDirName, 'loop_over_input', 'wrapped_error');
       const workflowPath = join(scenarioDir, 'workflow.yaml');
       const workflowEngine = await WorkflowEngineFactory.createFromFilePath(workflowPath, {
